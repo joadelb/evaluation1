6bb773744380621d28ea9d49b3c3effef553c70e
visual_community@outlook.com <visual_community@outlook.com>
2019-09-27 16:39:56 -0400

    Prueba de  servidor

---
+++ webEval/webEval/Models/Students.cs
@@ -0,0 +1,23 @@
+﻿
+namespace webEval.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public enum Sex
+    { male,female,gay
+    }
+    public class Students
+    {
+        [Key]
+        public int studentID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public Sex Gender { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime birthday { get; set; }
+    }
+}
